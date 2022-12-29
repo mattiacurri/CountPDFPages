53d52ee4bde22d2d4d046e6dcd17ea44099e6160
Mattia <mattia@pop-os.localdomain>
2022-12-29 11:17:11 +0100

    Aggiunte info a schermo su quali file vengono considerati per il calcolo totale delle pagine

--- countNumPDFPages.py
+++ countNumPDFPages.py
@@ -3,30 +3,26 @@ import os
 import sys
 
 n = len(sys.argv)
-# print("\nArguments passed:", end = " ")
-# for i in range(1, n):
-    #print(sys.argv[i], end = " ")
-     
-
 sumDir = [-1] * n
 directory = [-1] * n
 
-# assign directory
 for i in range(1, n):
+	print()
+	print("Path number", i)
 	sum = 0
 	directory[i] = sys.argv[i]
 	
 	if os.path.exists(directory[i]):
-		# iterate over files in directory
-		for filename in os.listdir(directory[i]):
+	    	for filename in os.listdir(directory[i]):
 	    		f = os.path.join(directory[i], filename)
 	    		if f.endswith('.pdf'):
 	    			file = open(f, 'rb')
 	    			readpdf = PyPDF2.PdfReader(file)
 	    			sum += len(readpdf.pages)
+		    		print("File:", f, "Num pages:", len(readpdf.pages))
 	    			sumDir[i] = sum
 	else:
-		print("L'argomento numero", i, "non è un percorso esistente")
+		print("Argument number", i, "not found")
 
-filtered_array = [x for x in sumDir if x != -1]
-print(filtered_array)
+filteredSum = [x for x in sumDir if x != -1]
+print(filteredSum)
