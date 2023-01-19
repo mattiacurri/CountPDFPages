670beac61f0ddd292824c9de69f8e067719604e0
mattiacurri <mattia2002@gmail.com>
2023-01-19 21:21:59 +0100

    Print of files cleaned

--- countNumPDFPages.py
+++ countNumPDFPages.py
@@ -7,22 +7,22 @@ sumDir = [-1] * n
 directory = [-1] * n
 
 for i in range(1, n):
-	print()
-	print("Path number", i)
-	sum = 0
-	directory[i] = sys.argv[i]
-	
-	if os.path.exists(directory[i]):
-	    	for filename in os.listdir(directory[i]):
-	    		f = os.path.join(directory[i], filename)
-	    		if f.endswith('.pdf'):
-	    			file = open(f, 'rb')
-	    			readpdf = PyPDF2.PdfReader(file)
-	    			sum += len(readpdf.pages)
-		    		print("File:", f, "Num pages:", len(readpdf.pages))
-	    			sumDir[i] = sum
-	else:
-		print("Argument number", i, "not found")
+    print()
+    print("Path number", i)
+    sum = 0
+    directory[i] = sys.argv[i]
+
+    if os.path.exists(directory[i]):
+        for filename in os.listdir(directory[i]):
+            f = os.path.join(directory[i], filename)
+            if f.endswith('.pdf'):
+                file = open(f, 'rb')
+                readpdf = PyPDF2.PdfReader(file)
+                sum += len(readpdf.pages)
+                print("File:", f.replace(directory[i] + "\\", ""), "Num pages:", len(readpdf.pages))
+                sumDir[i] = sum
+    else:
+        print("Argument number", i, "not found")
 
 filteredSum = [x for x in sumDir if x != -1]
 print(filteredSum)
